c3544a273182a506be39a988ed620ac6c482b07d
Jekabs Kulis <jekabs.kulis@gmail.com>
2026-03-14 18:24:07 +0200

    Added keyboard input, movable snake head

--- main.c
+++ main.c
@@ -2,45 +2,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <conio.h>//Windows only
 
 //Clears the old frame by printing empty lines.
 void clearFrame()
 {
-    int i;
-    for(i = 0; i < 50; i++)
-    {
-        printf("\n");
-    }
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
 
+//Checks for keyboard input until finds a valid key, ignores all other keys.
+char keyboardInput()
+{
+    char key;
+    if(kbhit())
+    {
+        key = getch();
+    }
+    else
+    {
+        return '0';
+    }
+    if(key!='w' && key !='s' && key!='d' && key!='a')
+    {
+        keyboardInput();
+    }
+    return key;
+}
 
 int main()
 {
-    char background[10][21] = {
-    {"...................."},
-    {"...................."},
-    {"...................."},
-    {"...................."},
-    {"...................."},
-    {"...................."},
-    {"...................."},
-    {"...................."},
-    {"...................."},
-    {"...................."},
+    char screen[20][41] = {
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"},
+    {"........................................"}
     };
+    int headPosX = 5;
+    int headPosY = 5;
     int i;
-    time_t t0 = time(NULL);
+    char key;
+    char pressedValidKey;
+    char head = 'O';
+    clock_t c0 = clock() / (CLOCKS_PER_SEC / 1000);
     while(1)
     {
-
-        time_t t1 = time(NULL);
-        if(difftime(t1, t0)>0.1)
+        clock_t c1 = clock() / (CLOCKS_PER_SEC / 1000);
+        //New frame every 400 milliseconds.
+        if(c1 - c0 > 400)
         {
-            t0=t1;
+            c0=c1;
             clearFrame();
-            for(i = 0; i < 10; i++)
+
+            key = keyboardInput();
+            //Saves the valid key into a different variable so the snake keeps moving.
+            if(key=='w' || key=='s' || key=='d' || key=='a')
+                pressedValidKey = key;
+
+            if(pressedValidKey=='w')
+                headPosY--;
+            else if(pressedValidKey=='s')
+                headPosY++;
+            else if(pressedValidKey=='d')
+                headPosX++;
+            else if(pressedValidKey=='a')
+                headPosX--;
+
+            //Resets the position of the head if it goes outside of the screen.
+            if(headPosX == 40)
+                headPosX = 0;
+            else if(headPosX == -1)
+                headPosX = 39;
+            if(headPosY == 20)
+                headPosY = 0;
+            else if(headPosY == -1)
+                headPosY = 19;
+
+            screen[headPosY][headPosX] = head;
+            for(i = 0; i < 20; i++)
             {
-                printf("%s\n", background[i]);
+                printf("%s\n", screen[i]);
             }
         }
     }
